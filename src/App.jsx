995fabd0cced31237c61a5870af8c0bd74fcc49d
Prajwal Kumar <kumpraj@gmail.com>
2023-04-25 14:00:25 +0530

    header,footer,textInput with validation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { Container, Box } from "@chakra-ui/react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import TextInput from "./components/TextInput"
 
 const App = () => {
   return (
-    <>
-      <h1>My App</h1>
-    </>
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={30}>
+      <Container maxW='3xl' centerContent>
+        <Header/>
+        <TextInput/>
+        <Footer/>
+      </Container>
+    </Box>
   )
 }
 
